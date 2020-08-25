06dffac5b11fe43211ab9db6377dbbb21e42fa2f
luoxiang723 <515088980@qq.com>
2020-08-25 11:32:54 +0800

    s

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ App({
     // baseImgUrl:'http://erp.jhdd.cn/',
     //  partnerCode: '1029',
     //  baseImgUrl:'http://39.98.164.194:8081/',
-     partnerCode: '1000', 
-     baseImgUrl: 'http://mmj.zksr.cn:8888/',
+    //  partnerCode: '1000', 
+    //  baseImgUrl: 'http://mmj.zksr.cn:8888/',
     // 15576615400 
     // partnerCode: '1036',
     // baseImgUrl: 'http://erp.yhfws.cn/',
@@ -23,7 +23,7 @@ App({
     
     // partnerCode: '10111',
     // baseImgUrl:'http://b2berp.goyounglife.com/',
-    // partnerCode: '1053',
+    partnerCode: '1053',
 
     // partnerCode: '1039',
     
